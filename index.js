71892e538556d287c7f3bd1d6e3a14a6c65daccc
Jony <jony378892@gmail.com>
2025-12-14 20:36:34 +0600

    Added products route for fetching all products

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +8,64 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/", (req, res) => {
-  res.send("Invalid route");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.aopfsxd.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("fabrico");
+    const usersCollection = db.collection("users");
+    const productsCollection = db.collection("products");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExists = await userCollection.findOne({ email });
+
+      if (userExists) {
+        return res.send({ message: "user exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const products = await productsCollection.find({}).toArray();
+      res.send(products);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = {};
+      if (id) {
+        query._id = new ObjectId(id);
+      }
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+
+    console.log("Successfully connected to mongoDB");
+  } finally {
+    //
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Invalid or unknown route");
 });
 
 app.listen(port, () => {
